f166e1a95423209d3650ba958274e82f583a3e9e
Farhan <famagung>
2018-04-12 13:21:02 +0700

    Recycler View error
    Signed-off-by: Farhan <famagung>

--- app/src/main/java/com/smktelkom/www/ta/Adapter/BarangAdapter.java
+++ app/src/main/java/com/smktelkom/www/ta/Adapter/BarangAdapter.java
@@ -1,11 +1,16 @@
 package com.smktelkom.www.ta.Adapter;
 
-import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.smktelkom.www.ta.Model.BarangModel;
+import com.smktelkom.www.ta.R;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,32 +18,43 @@ import android.widget.TextView;
  */
 
 public class BarangAdapter extends RecyclerView.Adapter<BarangAdapter.ViewHolder> {
+    ArrayList<BarangModel> barangModelList;
+
+    public BarangAdapter(ArrayList<BarangModel> hotelList) {
+        this.barangModelList = hotelList;
+    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.basic_main_list, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        BarangModel barangModel = barangModelList.get(position);
+        holder.tvBarang.setText(barangModel.barang);
+        holder.tvTanggal.setText(barangModel.tanggal);
+        holder.ivFoto.setImageDrawable(barangModel.foto);
     }
 
     @Override
     public int getItemCount() {
+        if (barangModelList != null)
+            return barangModelList.size();
         return 0;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public String tvbarang, tvkelas, tvtanggal;
-        public Drawable ivfoto;
+        public TextView tvBarang, tvTanggal;
+        public ImageView ivFoto;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            ivfoto = (ImageView) itemView.findViewById(R.id.imageView);
-            tvbarang = (TextView) itemView.findViewById(R.id.textViewBarang);
-            tvkelas = (TextView) itemView.findViewById(R.id.textViewKelas);
-            tvtanggal = (TextView) itemView.findViewById(R.id.textViewTanggal);
+            ivFoto = itemView.findViewById(R.id.imageView);
+            tvBarang = itemView.findViewById(R.id.textViewBarang);
+            tvTanggal = itemView.findViewById(R.id.textViewTanggal);
         }
     }
 }
--- app/src/main/java/com/smktelkom/www/ta/MainList.java
+++ app/src/main/java/com/smktelkom/www/ta/MainList.java
@@ -1,13 +1,49 @@
 package com.smktelkom.www.ta;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.smktelkom.www.ta.Adapter.BarangAdapter;
+import com.smktelkom.www.ta.Model.BarangModel;
+
+import java.util.ArrayList;
 
 public class MainList extends AppCompatActivity {
+    ArrayList<BarangModel> mList = new ArrayList<>();
+    BarangAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new BarangAdapter(mList);
+        fillData();
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    private void fillData() {
+        Resources resources = getResources();
+        String[] arBarang = resources.getStringArray(R.array.places);
+        String[] arTanggal = resources.getStringArray(R.array.place_desc);
+        TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+        Drawable[] arFoto = new Drawable[a.length()];
+        for (int i = 0; i < arFoto.length; i++) {
+            arFoto[i] = a.getDrawable(i);
+
+        }
+        for (int i = 0; i < arBarang.length; i++) {
+            mList.add(new BarangModel(arBarang[i], arTanggal[i], arFoto[i]));
+        }
+        mAdapter.notifyDataSetChanged();
     }
 }
+
